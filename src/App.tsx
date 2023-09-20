b0fc345270c3008fbd736bf957869b830c6b844e
chrisvasqm <christianvasquezmelo@gmail.com>
2023-09-19 22:50:52 -0400

    Display list

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import { Heading } from '@chakra-ui/react';
 
 function App() {
+  const tasks = [
+    { id: 1, name: 'Task 1', isChecked: false },
+    { id: 2, name: 'Task 2', isChecked: true }
+  ];
+
   return (
     <>
       <Heading>ToDo App</Heading>
+
+      <ul>{tasks && tasks.map(task => <li key={task.id}>{task.name}</li>)}</ul>
     </>
   );
 }
